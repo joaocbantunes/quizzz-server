745ebf85c0c7323641cb4475115b3d92cf97d31b
joaocbantunes <joaocbantunes@gmail.com>
2022-06-06 18:01:41 +0100

    monday

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
+const fileUploader = require("../config/cloudinary.config");
 
 router.get("/user", (req, res, next) => {
   const { _id } = req.payload;
-  console.log(req.payload)
+  console.log(req.payload);
   User.findById(_id)
     .then((response) => res.json(response))
     .catch((err) => res.json(err));
@@ -17,34 +18,38 @@ router.delete("/user/:userId/delete", (req, res, next) => {
     .catch((err) => res.json(err));
 });
 
-router.put("/user/:userId", (req, res, next) => {
-  const { userId } = req.params;
-  const { username, email, password } = req.body;
-  if (req.file) {
-    User.findByIdAndUpdate(
-      userId,
-      {
-        username,
-        email,
-        imageUrl: req.file.path,
-      },
-      { new: true }
-    )
-      .then((response) => res.json(response))
-      .catch((err) => res.status(400).json({ message: "No user updated" }));
-  } else {
-    User.findByIdAndUpdate(
-      userId,
-      {
-        username,
-        email,
-      },
-      { new: true }
-    )
+router.put(
+  "/user/:userId",
+  fileUploader.single("imageUrl"),
+  (req, res, next) => {
+    const { userId } = req.params;
+    const { username, email } = req.body;
+    if (req.file) {
+      User.findByIdAndUpdate(
+        userId,
+        {
+          username,
+          email,
+          imageUrl: req.file.path,
+        },
+        { new: true }
+      )
+        .then((response) => res.json(response, req.file.path))
+        .catch((err) => res.status(400).json({ message: "No user updated" }));
+    } else {
+      User.findByIdAndUpdate(
+        userId,
+        {
+          username,
+          email,
+        },
+        { new: true }
+      )
 
-      .then((response) => res.json(response))
-      .catch((err) => res.status(400).json({ message: "No user updated" }));
+        .then((response) => res.json(response))
+        .catch((err) => res.status(400).json({ message: "No user updated" }));
+    }
   }
-});
+);
 
 module.exports = router;
